a4d035e7738bd0c2c31ddf0b3a2917c64d783faa
VRxiaojie <1814014516@qq.com>
2025-06-09 16:39:21 +0800

    新增中断方式和DMA方式;新增FreeRTOS相关条件编译

--- Src/ina219.c
+++ Src/ina219.c
@@ -1,6 +1,38 @@
 #include "ina219.h"
 #include "ina219_conf.h"
+#if USE_FREERTOS == 1
+#include "FreeRTOS.h"
+#include "semphr.h"
 
+// 全局信号量句柄
+SemaphoreHandle_t INA219_Semaphore=NULL;
+#else
+volatile uint8_t INA219_data_ready = 0;
+#endif
+
+volatile enum {
+    INA219_IDLE,
+    INA219_READ_BUS_VOLTAGE,
+    INA219_READ_CURRENT,
+    INA219_READ_SHUNT_VOLTAGE,
+    INA219_READ_POWER
+} INA219_Request = INA219_IDLE;\
+volatile uint8_t INA219_data[2];
+volatile double INA219_last_result = 0.0;
+
+double getDataFromIT(void)
+{
+#if USE_FREERTOS == 1
+    if (xSemaphoreTake(INA219_Semaphore, 100) == pdTRUE) {
+        return INA219_last_result;
+    }
+#else
+    while (!INA219_data_ready) {
+    }
+    INA219_data_ready = 0;
+#endif
+    return INA219_last_result;
+}
 /**
  * @brief 设置INA219配置寄存器
  * @param config 配置参数。请在ina219_conf.h中修改宏INA219_CONFIG
@@ -26,6 +58,12 @@ void INA219_setCalibration(uint16_t calibration)
  */
 void INA219_Init(void)
 {
+#if USE_FREERTOS == 1
+    if (INA219_Semaphore == NULL) {
+        INA219_Semaphore = xSemaphoreCreateBinary();
+    }
+#endif
+
     INA219_setConfig(INA219_CONFIG);
     INA219_setCalibration(INA219_CALIBRATION_VALUE);
 }
@@ -78,4 +116,186 @@ double INA219_getPower(void)
     HAL_I2C_Mem_Read(&INA219_I2C_PORT, INA219_ADDR, POWER_REG, I2C_MEMADD_SIZE_8BIT, data, 2, 100);
     uint16_t rawdata = (data[0] << 8) | data[1];
     return (double)rawdata * 20 * INA219_CURRENT_LSB;
+}
+
+/**
+ * @brief 用中断方式获取母线电压
+ * @return 电压值(Unit:V)
+ */
+double INA219_getBusVoltage_IT(void)
+{
+    INA219_Request = INA219_READ_BUS_VOLTAGE;
+    HAL_I2C_Mem_Read_IT(&INA219_I2C_PORT, INA219_ADDR, BUS_VOLTAGE_REG, I2C_MEMADD_SIZE_8BIT, (uint8_t *)INA219_data, 2);
+    getDataFromIT();
+    return -1.0;
+}
+
+/**
+ * @brief 用中断方式获取采样电流
+ * @return 采样电流值(Unit:A)
+ */
+double INA219_getCurrent_IT(void)
+{
+    INA219_Request = INA219_READ_CURRENT;
+    HAL_I2C_Mem_Read_IT(&INA219_I2C_PORT, INA219_ADDR, CURRENT_REG, I2C_MEMADD_SIZE_8BIT, (uint8_t *)INA219_data, 2);
+    getDataFromIT();
+    return -1.0;
+}
+
+/**
+ * @brief 用中断方式获取分流电阻上的电压
+ * @return 分流电阻上的电压(Unit:V)
+ */
+double INA219_getShuntVoltage_IT(void)
+{
+    INA219_Request = INA219_READ_SHUNT_VOLTAGE;
+    HAL_I2C_Mem_Read_IT(&INA219_I2C_PORT, INA219_ADDR, SHUNT_VOLTAGE_REG, I2C_MEMADD_SIZE_8BIT, (uint8_t *)INA219_data, 2);
+    getDataFromIT();
+    return -1.0;
+}
+
+/**
+ * @brief 用中断方式从INA219内部功率寄存器获取功率
+ * @return 功率值(Unit:W)
+ */
+double INA219_getPower_IT(void)
+{
+    INA219_Request = INA219_READ_POWER;
+    HAL_I2C_Mem_Read_IT(&INA219_I2C_PORT, INA219_ADDR, POWER_REG, I2C_MEMADD_SIZE_8BIT, (uint8_t *)INA219_data, 2);
+    getDataFromIT();
+    return -1.0;
+}
+
+/**
+ * @brief 用DMA方式获取母线电压
+ * @return 电压值(Unit:V)
+ */
+double INA219_getBusVoltage_DMA(void)
+{
+    uint8_t data[2];
+    if (HAL_I2C_Mem_Read_DMA(&INA219_I2C_PORT, INA219_ADDR, BUS_VOLTAGE_REG, I2C_MEMADD_SIZE_8BIT, data, 2) != HAL_OK)
+    {
+        return -1.0;
+    }
+
+#if USE_FREERTOS == 1
+    if (xSemaphoreTake(INA219_Semaphore, 100) != pdTRUE)
+    {
+        return -2.0;
+    }
+#endif
+
+    uint16_t rawdata = ((data[0] << 8) | data[1]) >> 3;
+    return (double)rawdata * INA219_VOLT_LSB;
+}
+
+/**
+ * @brief 用DMA方式获取采样电流
+ * @return 采样电流值(Unit:A)
+ */
+double INA219_getCurrent_DMA(void)
+{
+    uint8_t data[2];
+    if (HAL_I2C_Mem_Read_DMA(&INA219_I2C_PORT, INA219_ADDR, CURRENT_REG, I2C_MEMADD_SIZE_8BIT, data, 2) != HAL_OK)
+    {
+        return -1.0;
+    }
+#if USE_FREERTOS == 1
+    if (xSemaphoreTake(INA219_Semaphore, 100) != pdTRUE)
+    {
+        return -2.0;
+    }
+#endif
+
+    int16_t rawdata = (int16_t)((data[0] << 8) | data[1]);
+    return (double)rawdata * INA219_CURRENT_LSB;
+}
+
+/**
+ * @brief 用DMA方式获取分流电阻上的电压
+ * @return 分流电阻上的电压(Unit:V)
+ */
+double INA219_getShuntVoltage_DMA(void)
+{
+    uint8_t data[2];
+    if (HAL_I2C_Mem_Read_DMA(&INA219_I2C_PORT, INA219_ADDR, SHUNT_VOLTAGE_REG, I2C_MEMADD_SIZE_8BIT, data, 2) != HAL_OK)
+    {
+        return -1.0;
+    }
+#if USE_FREERTOS == 1
+    if (xSemaphoreTake(INA219_Semaphore, 100) != pdTRUE)
+    {
+        return -2.0;
+    }
+#endif
+    int16_t rawdata = (int16_t)((data[0] << 8) | data[1]);
+    return (double)rawdata * 0.00001;
+}
+
+/**
+ * @brief 用DMA方式从INA219内部功率寄存器获取功率
+ * @return 功率值(Unit:W)
+ */
+double INA219_getPower_DMA(void)
+{
+    uint8_t data[2];
+
+    if (HAL_I2C_Mem_Read_DMA(&INA219_I2C_PORT, INA219_ADDR, POWER_REG, I2C_MEMADD_SIZE_8BIT, data, 2) != HAL_OK)
+    {
+        return -1.0;
+    }
+#if USE_FREERTOS == 1
+    if (xSemaphoreTake(INA219_Semaphore, 100) != pdTRUE)
+    {
+        return -2.0;
+    }
+#endif
+    uint16_t rawdata = (data[0] << 8) | data[1];
+    return (double)rawdata * 20 * INA219_CURRENT_LSB;
+}
+
+void HAL_I2C_MemRxCpltCallback(I2C_HandleTypeDef *hi2c)
+{
+    if(hi2c == &INA219_I2C_PORT) {
+#if USE_FREERTOS == 1
+        BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+#endif
+
+        switch(INA219_Request)
+        {
+            case INA219_READ_BUS_VOLTAGE:
+            {
+                uint16_t raw = ((INA219_data[0] << 8) | INA219_data[1]) >> 3;
+                INA219_last_result = (double)raw * INA219_VOLT_LSB;
+                break;
+            }
+            case INA219_READ_CURRENT:
+            {
+                int16_t raw = (int16_t)((INA219_data[0] << 8) | INA219_data[1]);
+                INA219_last_result = (double)raw * INA219_CURRENT_LSB;
+                break;
+            }
+            case INA219_READ_SHUNT_VOLTAGE:
+            {
+                int16_t raw = (int16_t)((INA219_data[0] << 8) | INA219_data[1]);
+                INA219_last_result = (double)raw * 0.00001;
+                break;
+            }
+            case INA219_READ_POWER:
+            {
+                uint16_t raw = (INA219_data[0] << 8) | INA219_data[1];
+                INA219_last_result = (double)raw * 20 * INA219_CURRENT_LSB;
+                break;
+            }
+            default:
+                break;
+        }
+        INA219_Request = INA219_IDLE;
+#if USE_FREERTOS == 1
+        xSemaphoreGiveFromISR(INA219_Semaphore, &xHigherPriorityTaskWoken);
+        portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+#else
+        INA219_data_ready = 1;
+#endif
+    }
 }
